1275eaab2ec2d542431dd853e92d3f127deaf5e6
Yuri <77989191+yuribmontez@users.noreply.github.com>
2021-03-24 19:14:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,32 +3,65 @@ var msg2 = document.getElementById('mensagem2');
 var msg3 = document.getElementById('mensagem3');
 
 var resposta = Math.floor(Math.random() * 100) + 0;
-var chutes = 0;
+var chutes = 7;
 var numerosChutados = [];
+var chutesGastos = 0;
+
+function jogarNovamente() {
+    location.reload();
+}
 
 function jogar() {
+    alert(resposta);
     var chute = document.getElementById('chute').value;
+    chutes -= 1;
+    chutesGastos += 1;
     if (chute < 0 || chute > 100) {
-        alert('Por favor escolha um número entre 0 e 100.');
+        alert('Por favor escolha um número entre 0 e 100.')
     } else {
         numerosChutados.push(chute);
-        chutes += 1;
+        while (chutes >= 1) {
+            if (chute < resposta) {
+                msg1.textContent = 'Esse é seu melhor chute? Tente um número maior.';
+                msg2.textContent = 'Nº de chutes: ' + chutes;
+                msg3.textContent = 'Números chutados: ' + numerosChutados;
+                break;
+            }
+            else if (chute > resposta) {
+                msg1.textContent = 'Chutou alto demais. Tente um número menor.';
+                msg2.textContent = 'Nº de chutes: ' + chutes;
+                msg3.textContent = 'Números chutados: ' + numerosChutados;
+                break;
+            }
+            else if (chute == resposta) {
+                msg1.textContent = 'Boaaaaaaaa, você acertou. Como você conseguiu? :O';
+                msg2.textContent = 'A resposta era: ' + resposta;
+                msg3.textContent = 'Você acertou em: ' + chutesGastos + ' chutes';
+                document.getElementById('botao').style.display = 'none';
+                document.getElementById('chute').style.display = 'none';
+                document.getElementById('botao2').style.display = 'inline';
 
-        if (chute < resposta) {
-            msg1.textContent = "Esse é seu melhor chute? Tente um número maior.";
-            msg2.textContent = 'Nº de chutes: ' + chutes;
-            msg3.textContent = 'Números chutados: ' + numerosChutados;
+                break;
+            }
         }
-        else if (chute > resposta) {
-            msg1.textContent = "Chutou alto demais. Tente um número menor.";
-            msg2.textContent = 'Nº de chutes: ' + chutes;
+        if (chutes == 0) {
+            msg1.textContent = 'Erroooooooou feio, errooooooooou rude. O número era: ' + resposta;
+            msg2.textContent = '';
             msg3.textContent = 'Números chutados: ' + numerosChutados;
+            document.getElementById('chute').style.display = 'none';
+            document.getElementById('botao').style.display = 'none';
+            document.getElementById('botao2').style.display = 'inline';
         }
-        else if (chute == resposta) {
-            msg1.textContent = "Boaaaaaaaa, você acertou. Como você conseguiu? :O";
+        if (chute == resposta) {
+            msg1.textContent = 'Boaaaaaaaa, você acertou. Como você conseguiu? :O';
             msg2.textContent = 'A resposta era: ' + resposta;
-            msg3.textContent = 'Você acertou em: ' + chutes + ' chutes';
-            document.getElementById('botao').disabled = true;
+            msg3.textContent = 'Você acertou em: ' + chutesGastos + ' chutes';
+            document.getElementById('botao').style.display = 'none';
+            document.getElementById('chute').style.display = 'none';
+            document.getElementById('botao2').style.display = 'inline';
         }
     }
+
 }
+
+
